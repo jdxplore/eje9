612382bcf4e0c71724f3907777842f440e210a3c
José David González Pizarro <aa21.jdgonzalez78@iespabloserrano.com>
2025-11-13 08:49:57 +0100

    Src correction

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,9 @@
 public class Calculadora {
-    public  int sumar(int a, int b) {
+    public int sumar(int a, int b) {
         return a + b;
     }
-    public  int restar(int a, int b) {
+
+    public int restar(int a, int b) {
         return a - b;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
-
-        int a = 10;
-        int b = 5;
-
-        int suma = calc.sumar(a, b);
-        int resta = calc.restar(a, b);
-
-        System.out.println("Suma de " + a + " + " + b + " = " + suma);
-        System.out.println("Resta de " + a + " - " + b + " = " + resta);
-
+        System.out.println("Resultado suma: " + calc.sumar(2, 3));
+		System.out.println("Resultado resta: " + calc.restar(2, 3));
     }
 }
